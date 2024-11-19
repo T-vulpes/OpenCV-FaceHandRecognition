3a4166f61ae2bf8e79851bcdd2047ff612d2850c
Deniz <167799743+T-vulpes@users.noreply.github.com>
2024-11-19 18:14:47 +0300

    Update modelfin.py

--- Gesture Volume Control/modelfin.py
+++ Gesture Volume Control/modelfin.py
@@ -50,13 +50,13 @@ def main():
         img = detector.findHands(img)
         lmList = detector.findPosition(img)
         if len(lmList) != 0:
-            print(lmList[4])  # Başparmak ucunun koordinatlarını yazdırır
+            print(lmList[4])  
         cTime = time.time()
         fps = 1 / (cTime - pTime)
         pTime = cTime
         cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
         cv2.imshow("Image", img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):  # Çıkış için 'q' tuşu
+        if cv2.waitKey(1) & 0xFF == ord('q'):  
             break
 
 if __name__ == "__main__":
