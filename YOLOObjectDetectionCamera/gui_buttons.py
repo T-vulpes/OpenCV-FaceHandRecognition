41147b8a3a2e763bf99ab9f94c40c0f1112a66ec
Deniz <167799743+T-vulpes@users.noreply.github.com>
2024-11-22 16:19:07 +0300

    Update gui_buttons.py

--- YOLOObjectDetectionCamera/gui_buttons.py
+++ YOLOObjectDetectionCamera/gui_buttons.py
@@ -1,31 +1,24 @@
 import cv2
 import numpy as np
 
-
 class Buttons:
     def __init__(self):
-        # Font and appearance settings
         self.font = cv2.FONT_HERSHEY_PLAIN
         self.text_scale = 2
         self.text_thick = 2
         self.x_margin = 20
         self.y_margin = 15
-
-        # Buttons
         self.buttons = {}
         self.button_index = 0
 
-        # Generate colors
         self.colors = {"inactive": (200, 200, 200), "active": (0, 255, 0), "border": (0, 0, 255)}
         self.text_colors = {"inactive": (0, 0, 0), "active": (255, 255, 255)}
 
     def add_button(self, text, x, y):
-        # Calculate button size based on text
         textsize = cv2.getTextSize(text, self.font, self.text_scale, self.text_thick)[0]
         right_x = x + (self.x_margin * 2) + textsize[0]
         bottom_y = y + (self.y_margin * 2) + textsize[1]
 
-        # Add button to dictionary
         self.buttons[self.button_index] = {
             "text": text,
             "position": [x, y, right_x, bottom_y],
@@ -43,12 +36,9 @@ class Buttons:
             button_color = self.colors["active"] if active else self.colors["inactive"]
             text_color = self.text_colors["active"] if active else self.text_colors["inactive"]
             border_color = self.colors["border"]
-
-            # Draw button
             cv2.rectangle(frame, (x, y), (right_x, bottom_y), border_color, 2)  # Border
             cv2.rectangle(frame, (x + 2, y + 2), (right_x - 2, bottom_y - 2), button_color, -1)  # Inner fill
 
-            # Draw text
             cv2.putText(frame, button_text, (x + self.x_margin, bottom_y - self.y_margin),
                         self.font, self.text_scale, text_color, self.text_thick)
         return frame
