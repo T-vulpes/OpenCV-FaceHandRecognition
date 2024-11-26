b2ffde9bfa0ef78b50020145e6e2b46bee544375
Deniz <167799743+T-vulpes@users.noreply.github.com>
2024-11-26 15:26:50 +0300

    Update mainnnn.py

--- Body Landmarks Detection/mainnnn.py
+++ Body Landmarks Detection/mainnnn.py
@@ -18,7 +18,7 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
 
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        # Yüz 
+        # Face 
         if results.face_landmarks:
             mp_drawing.draw_landmarks(
                 image,
@@ -26,34 +26,29 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
                 landmark_drawing_spec=mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1)
             )
 
-        # Sağ el
+        # Right hand
         if results.right_hand_landmarks:
             mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
                                       mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
                                       mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2)
                                       )
 
-        # Sol el 
+        # Left hand
         if results.left_hand_landmarks:
             mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
                                       mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
                                       mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
                                       )
 
-        # Poz 
+        # posw 
         if results.pose_landmarks:
             mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
                                       mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
                                       mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
                                       )
-
-        # Çıktıyı göster
         cv2.imshow('Web kamera goruntusu', image)
-
-        # Çıkış tuşu
         if cv2.waitKey(10) & 0xFF == ord('t'):
             break
 
-# Kaynakları serbest bırak
 cap.release()
 cv2.destroyAllWindows()
