bdb68252ea9c3991974ca37e71ebf65ce717ef59
Deniz <167799743+T-vulpes@users.noreply.github.com>
2024-11-26 15:24:52 +0300

    holistic

---
+++ Body Landmarks Detection/mainnnn.py
@@ -0,0 +1,59 @@
+import mediapipe as mp
+import cv2
+
+mp_drawing = mp.solutions.drawing_utils
+mp_holistic = mp.solutions.holistic
+
+cap = cv2.VideoCapture(0)
+
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            print("Kameradan görüntü alınamadı.")
+            break
+
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = holistic.process(image)
+
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        # Yüz 
+        if results.face_landmarks:
+            mp_drawing.draw_landmarks(
+                image,
+                results.face_landmarks,
+                landmark_drawing_spec=mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1)
+            )
+
+        # Sağ el
+        if results.right_hand_landmarks:
+            mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                                      mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
+                                      mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2)
+                                      )
+
+        # Sol el 
+        if results.left_hand_landmarks:
+            mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                                      mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                                      mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
+                                      )
+
+        # Poz 
+        if results.pose_landmarks:
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                                      mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
+                                      mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                                      )
+
+        # Çıktıyı göster
+        cv2.imshow('Web kamera goruntusu', image)
+
+        # Çıkış tuşu
+        if cv2.waitKey(10) & 0xFF == ord('t'):
+            break
+
+# Kaynakları serbest bırak
+cap.release()
+cv2.destroyAllWindows()
