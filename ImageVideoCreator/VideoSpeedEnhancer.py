90ae3e29c8c3f3fab6b747cd825fe9b9b56397ab
Deniz <167799743+T-vulpes@users.noreply.github.com>
2024-12-07 15:36:22 +0300

    speedvideo

--- ImageVideoCreator/VideoSpeedEnhancer.py
+++ ImageVideoCreator/VideoSpeedEnhancer.py
@@ -1,29 +1,25 @@
 import cv2
 
 cap = cv2.VideoCapture("newvideo.mp4")
-
 cv2_fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 success, img = cap.read()
 
 if not success:
-    raise ValueError("Video okunamadı. Dosya yolunu kontrol edin veya video bozuk olabilir.")
+    raise ValueError("The video could not be read. Check the file path or the video may be corrupt.")
 
 size = (img.shape[1], img.shape[0])  
-video = cv2.VideoWriter("fasternewvideo.mp4", cv2_fourcc, 30, size)  # Output video adı, fourcc, fps, size
+video = cv2.VideoWriter("fasternewvideo.mp4", cv2_fourcc, 30, size)  
 
 while success:
     video.write(img)
-
     cv2.imshow("Image", img)
-
     if cv2.waitKey(1) & 0xFF == ord('t'):
         break
 
     success, img = cap.read()  
 
-# st bırak
 cap.release()
 video.release()
 cv2.destroyAllWindows()
 
-print("Yeni video başarıyla kaydedildi: fasternewvideo.mp4")
+print("New video saved successfully: fasternewvideo.mp4")
