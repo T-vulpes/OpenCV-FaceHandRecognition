bcaa443b11f229301622fbfcdcd67d3bd805979c
Deniz <167799743+T-vulpes@users.noreply.github.com>
2024-12-09 15:59:55 +0300

    objct

--- Object Movement Detection/main.py
+++ Object Movement Detection/main.py
@@ -10,12 +10,11 @@ while cap.isOpened():
     gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
     _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
-    dilated = cv2.dilate(thresh, None, iterations=3)  # daha iyi kontör çizimi için boşlukları doldur (dilation)
+    dilated = cv2.dilate(thresh, None, iterations=3) 
     contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in contours:
         (x, y, w, h) = cv2.boundingRect(contour)
-
         if cv2.contourArea(contour) < 900:
             continue
         cv2.rectangle(frame1, (x, y), (x + w, y + h), (0, 255, 0), 2)
