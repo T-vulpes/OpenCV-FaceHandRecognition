ffc1f1b68b393c1d9ae73e9a66e3d7829a3a04fd
Deniz <167799743+T-vulpes@users.noreply.github.com>
2024-12-11 14:08:20 +0300

    voiccc

--- Real-TimeObjectNarrator/voice.py
+++ Real-TimeObjectNarrator/voice.py
@@ -2,11 +2,9 @@ from pygame import mixer
 import time
 from gtts import gTTS
 
-# gTTS nesnesi oluşturma
-tts = gTTS("hello my name is tvlps")  # Burada bir gTTS nesnesi oluşturuluyor
-tts.save("hello.mp3")  # gTTS nesnesini kaydediyoruz
+tts = gTTS("hello my name is tvlps")  
+tts.save("hello.mp3")  
 
-# Pygame ile ses çalma
 mixer.init()
 mixer.music.load("hello.mp3")
 mixer.music.play()
