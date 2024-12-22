0f22bf2b0d7cfad1ae3b828fec55b5d125dbdacf
Deniz <167799743+T-vulpes@users.noreply.github.com>
2024-12-22 14:48:40 +0300

    x

---
+++ Basic Motion Detection/main.py
@@ -0,0 +1,27 @@
+import cv2
+import time
+import winsound
+
+cam = cv2.VideoCapture(0)
+
+while True:
+    _, im = cam.read()
+    _, im2 = cam.read()
+
+    absdiff = cv2.absdiff(im, im2)
+    gray = cv2.cvtColor(absdiff, cv2.COLOR_BGR2GRAY)
+    _, thresh = cv2.threshold(gray, 20, 255, cv2.THRESH_BINARY)
+
+    contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for x in contours:
+        if cv2.contourArea(x) < 5000:
+            continue
+        winsound.Beep(500, 100)
+
+    cv2.imshow("Camera", thresh)
+
+    if cv2.waitKey(10) == ord('t'):  # 'T' tuşuna basıldığında çık
+        break
+
+cam.release()
+cv2.destroyAllWindows()
