bba022a182e4745d435c94e908641427e304d214
Deniz <167799743+T-vulpes@users.noreply.github.com>
2024-12-31 15:20:25 +0300

    Update main.py

--- HorizontalStitch/main.py
+++ HorizontalStitch/main.py
@@ -18,16 +18,13 @@ def main():
         print(f"Görüntü yüklenemedi: {image2_path}")
         return
 
-    # Görüntülerin boyutlarını eşitle
     height = max(image1.shape[0], image2.shape[0])
     width = max(image1.shape[1], image2.shape[1])
     resized_image1 = cv2.resize(image1, (width, height))
     resized_image2 = cv2.resize(image2, (width, height))
 
-    # Görüntüleri yatay olarak birleştir
     combined_horizontal = combine_images_horizontally([resized_image1, resized_image2])
 
-    # Sonuçları kaydet ve göster
     cv2.imwrite("combined_horizontal.jpg", combined_horizontal)
 
     cv2.imshow("Horizontal Combination", combined_horizontal)
