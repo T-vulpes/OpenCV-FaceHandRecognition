d34ae8df1847778bd0c80fcd4c1791b401188997
Deniz <167799743+T-vulpes@users.noreply.github.com>
2025-01-08 15:56:54 +0300

    Update main.py

--- Eye-Controlled Cursor/main.py
+++ Eye-Controlled Cursor/main.py
@@ -11,26 +11,21 @@ screen_width, screen_height = pyautogui.size()
 # Önceki koordinatları takip etmek için değişkenler
 previous_mouse_x, previous_mouse_y = 0, 0
 
-# Hareket eşik değeri
 THRESHOLD = 15
-
 while True:
     ret, frame = cam.read()
     if not ret:
         break
 
-    # Görüntüyü aynalayarak ve gri tonlara çevirerek işle
     frame = cv2.flip(frame, 1)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Yüz algılama
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
     for (x, y, w, h) in faces:
         # Göz bölgesine odaklan (yalnızca üst yarı)
         roi_gray = gray[y:y + h // 2, x:x + w]
         roi_color = frame[y:y + h // 2, x:x + w]
 
-        # Göz algılama
         eyes = eye_cascade.detectMultiScale(roi_gray, scaleFactor=1.1, minNeighbors=4)
         for (ex, ey, ew, eh) in eyes:
             # Gözün merkezi
